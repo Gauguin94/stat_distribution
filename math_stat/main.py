556e52e7c73c9f342916edfb73a062301e2bb56f
Gauguin94 <98927470+Gauguin94@users.noreply.github.com>
2022-05-24 15:08:35 +0900

    Create main.py

---
+++ math_stat/main.py
@@ -0,0 +1,7 @@
+from linkDist import*
+
+distn_list = ['B','NB','G','P','r','X_2','Beta','N']
+
+if __name__ == "__main__":
+    select_dist = linkToDist()
+    select_dist.switch('B') # or u can use "select_dist.switch(distn_list[0])"
